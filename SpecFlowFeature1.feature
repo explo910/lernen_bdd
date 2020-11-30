ac8d4362dd9cb01dc41cf80da15e2d440ae23002
Michael <Scheidl>
2020-11-30 23:56:53 +0100

    adsfasdf

---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,16 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Sinus
+	Given the first number is pi
+	When the sinus is selected
+	Then the result should be 1
+
+
+Scenario: Cosinus
+	Given the number is pi
+	When the cosinus is selected
+	Then the result should be 0
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace lernen_bdd
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"the first number is pi")]
+        public void GivenTheFirstNumberIsPi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the number is pi")]
+        public void GivenTheNumberIsPi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the sinus is selected")]
+        public void WhenTheSinusIsSelected()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the cosinus is selected")]
+        public void WhenTheCosinusIsSelected()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
